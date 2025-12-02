aafccd2d98c603d073fd25eb1c8543343854c045
ErikArgemi <erik.argemi@estudiantat.upc.edu>
2025-12-02 14:20:19 +0100

    PhysicEntity header

---
+++ Source/PhysicEntity.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ModulePhysics.h"
+#include "ModuleGame.h"
+
+class PhysicEntity
+{
+public:
+
+	PhysicEntity(PhysBody* _body, Module* _listener)
+		: physBody(_body)
+		, listener(_listener)
+	{
+		physBody->listener = listener;
+	}
+
+	virtual ~PhysicEntity() = default;
+	virtual void Update() = 0;
+
+	virtual int RayHit(vec2<int> ray, vec2<int> mouse, vec2<float>& normal)
+	{
+		return 0;
+	}
+
+	PhysBody* physBody;
+	Module* listener;
+	int type;
+
+};
