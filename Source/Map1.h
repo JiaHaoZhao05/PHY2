9cc573b123aa1fe3cf448e549535cae11ab24775
ErikArgemi <erik.argemi@estudiantat.upc.edu>
2025-12-11 20:13:49 +0100

    .

--- Source/Map1.h
+++ Source/Map1.h
@@ -6,9 +6,9 @@
 class Map1 {
 public:
 	Map1();
-	Map1(std::vector<b2ChainShape> _mapChain);
+	Map1(b2ChainShape _mapChain);
 	~Map1();
 
 private:
-	std::vector<b2ChainShape> mapChain;
+	b2ChainShape mapChain1;
 };
