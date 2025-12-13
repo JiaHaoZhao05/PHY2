2dbedf694893980829fbb25f881b07cca0916ad1
OscarJimenez25 <196553974+OscarJimenez25@users.noreply.github.com>
2025-12-13 13:49:47 +0100

    Update Enemy.h

--- Source/Enemy.h
+++ Source/Enemy.h
@@ -9,7 +9,7 @@
 class Enemy :public Car {
 public:
 	Enemy(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture, float friction)
-		: Car(physics->CreateRectangle(_x, _y, 5, 8, ENEMY), _listener, EntityType::ENEMY)
+		: Car(physics->CreateRectangle(_x, _y, 5, 8,0.6, ENEMY), _listener, EntityType::ENEMY)
 		, texture(_texture)
 	{
 
