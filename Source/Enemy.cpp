a2a4f84b01b341c1d7017483f6418d1406ebf6f4
JiaHaoZhao05 <jia.hao.zhao@estudiantat.upc.edu>
2025-12-15 12:10:12 +0100

    Ai takes into account enemy atributes, new enemy

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -18,6 +18,8 @@ bool Enemy::Start()
 	LOG("Loading enemy");
 	//centerLine = App->scenario->map1->GetCenterLine();
 	texture = LoadTexture("Assets/Textures/enemy.png");
+    speed = 5;
+    angle = 0;
 	return true;
 }
 // Unload assets
@@ -32,7 +34,7 @@ bool Enemy::CleanUp()
 bool Enemy::Update() {
 	float dt = 1.0f / 60.0f; // or your actual timestep
 	GroundFriction();
-	ai.Update(this, centerLine, dt);
+	ai.Update(this, centerLine, dt, speed, maxspeed, torque, maxtorque);
 	Draw();
 	return true;;
 }
@@ -66,7 +68,7 @@ float PID::step(float error, float dt) {
 	return u;
 }
 
-void AIController::Update(Car* car, const std::vector<b2Vec2>& waypoints, float dt) {
+void AIController::Update(Car* car, const std::vector<b2Vec2>& waypoints, float dt, float speed, float maxspeed, float torque, float maxtorque) {
     b2Vec2 pos = car->physBody->body->GetPosition();
     pos = b2Vec2{ (float)METERS_TO_PIXELS(pos.x), (float)METERS_TO_PIXELS(pos.y) };
     float angle = car->physBody->body->GetAngle();
@@ -87,6 +89,9 @@ void AIController::Update(Car* car, const std::vector<b2Vec2>& waypoints, float 
     float desiredAngle = atan2f(dir.y, dir.x);
 
     // Steering error
+    if (desiredAngle > 180 * DEGTORAD) {
+        desiredAngle = 180 * DEGTORAD - desiredAngle;
+    }
     float steerError = fmodf(desiredAngle - angle + b2_pi, 2 * b2_pi) - b2_pi / 2;
     float steerCmd = steerPID.step(steerError, dt);
 
@@ -96,12 +101,32 @@ void AIController::Update(Car* car, const std::vector<b2Vec2>& waypoints, float 
     b2Vec2 vel = car->physBody->body->GetLinearVelocity();
     b2Vec2 forward = car->physBody->body->GetWorldVector(b2Vec2(0.0f, 1.0f));
     float vCurrent = b2Dot(vel, forward);
-    float throttleCmd = throttlePID.step(vTarget - vCurrent, dt);
+    float throttleCmd = throttlePID.step(speed - vCurrent, dt);
 
     // Apply forces
-    if (car->physBody->body->GetLinearVelocity().Length() < maxVTarget) {
+    if (car->physBody->body->GetLinearVelocity().Length() < maxspeed) {
         car->physBody->body->ApplyForceToCenter(-throttleCmd * forward, true);
     }
-    car->physBody->body->ApplyTorque(steerCmd, true);
-
+    if (car->physBody->body->GetAngularVelocity() < maxtorque) {
+        if (steerCmd < 0) {
+            car->physBody->body->ApplyTorque(-torque, true);
+        }
+        else if(steerCmd > 0){
+            car->physBody->body->ApplyTorque(torque, true);
+        }
+    }
+}
+EnemyTooth::~EnemyTooth()
+{
+}
+bool EnemyTooth::Start(){
+    LOG("Loading enemy");
+    texture = LoadTexture("Assets/Textures/enemy2.png");
+    speed = 10;
+    maxspeed = 22;
+    angle = 0;
+    torque = 3;
+    maxtorque = 6;
+    brake = 2;
+    return true;
 }
